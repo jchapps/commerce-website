afdf286f5c3463c1db6dcf3274884501e28f1e15
Josh Chappelow <jdchappelow@gmail.com>
2023-02-22 21:08:02 +0900

    feat: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
 
   return (
-    <div className="app">
-      Test
+    <div className="app bg-gray-50">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/scenes/navbar/link.tsx
@@ -0,0 +1,25 @@
+import { SelectedPage } from "@/shared/types";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const link = ({ page, selectedPage, setSelectedPage }: Props) => {
+  const lowerCasePages = page.toLowerCase().replace(/ /g, "") as SelectedPage;
+  return (
+    <AnchorLink
+      href={`#${lowerCasePages}`}
+      onClick={() => setSelectedPage(lowerCasePages)}
+      className={`${
+        selectedPage === lowerCasePages ? "text-primary-500" : ""
+      } tranisiton duration-500 hover:text-primary-300`}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+export default link;
